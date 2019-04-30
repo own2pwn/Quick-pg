464c5fe962f04351e21692f24e04d3af18bb4992
Evgeniy <7850039+own2pwn@users.noreply.github.com>
2019-04-30 08:15:49 +0300

    Popup sizing

--- Quick-pg/Quick-pg/Module/Playground/PlaygroundController.swift
+++ Quick-pg/Quick-pg/Module/Playground/PlaygroundController.swift
@@ -17,7 +17,7 @@ public enum QuickViewType {
 
 final class PlaygroundViewHolder {}
 
-final class InteractivePopup: EPCardView {
+final class InteractivePopup: EPShadowCardView {
     // MARK: - Views
 
     private let doneButton: EPTitledButton = {
@@ -43,28 +43,55 @@ final class InteractivePopup: EPCardView {
         return btn
     }()
 
+    private let textField: StaticPlaceholderTextField = {
+        let textField = StaticPlaceholderTextField()
+        textField.font = Fonts.CoreSansARMedium(ofSize: 18)
+
+        textField.textColor = #colorLiteral(red: 0.9309999943, green: 0.9462000728, blue: 0.9499999881, alpha: 1)
+        textField.placeholderTextColor = #colorLiteral(red: 0.7409999967, green: 0.7643999457, blue: 0.7799999118, alpha: 1)
+        textField.backgroundColor = .clear // #colorLiteral(red: 0.436576277, green: 0.8080026507, blue: 0.5136813521, alpha: 1)
+
+        textField.placeholder = "View internal name"
+        textField.update()
+
+        return textField
+    }()
+
     override var views: [UIView] {
-        return [doneButton]
+        return [doneButton, textField]
     }
 
     // MARK: - Setup
 
     override func setup() {
-        backgroundColor = #colorLiteral(red: 0.1499999464, green: 0.1499999464, blue: 0.1499999464, alpha: 1)
+        contentView.backgroundColor = #colorLiteral(red: 0.1499999464, green: 0.1499999464, blue: 0.1499999464, alpha: 1)
         cornerRadius = 6
+
+        shadow = Shadow(
+            color: #colorLiteral(red: 0.1699999571, green: 0.1699999571, blue: 0.1699999571, alpha: 1), radius: 8,
+            offset: CGSize(width: 0, height: 0.75),
+            opacity: 0.5
+        )
     }
 
     // MARK: - Layout
 
     override func layout() {
-        pin.height(280).width(540)
-
         doneButton.pin
             .top()
             .end()
             .margin(8)
             .height(32)
             .sizeToFit(.height)
+
+        textField.pin
+            .height(32)
+            .below(of: doneButton)
+            .marginTop(32)
+            .horizontally(10)
+
+        pin.height(textField.frame.maxY + 24)
+            .aspectRatio(1 + (1 / 1.2))
     }
 }
 
@@ -137,11 +164,13 @@ final class PlaygroundController: EYController {
             .end(view.pin.safeArea)
             .marginEnd(24)
 
-        popupView
-            .layoutIfNeeded()
+        layoutPopup()
+    }
 
-        popupView.pin
-            .center()
+    private func layoutPopup() {
+        // popupView.pin.width(view.frame.width / 3.2)
+        popupView.layoutIfNeeded()
+        popupView.pin.center()
     }
 }
 
